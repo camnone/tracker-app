23239fc07b579fe78d6f0afe7d57b0bd088dcba1
Artyom Tazhetdinov <artemtazhetdinov4@gmail.com>
2023-06-18 20:00:47 +0300

    update123

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import trackerRoute from "./routes/tracker_route.js";
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  const app = express();
+
+  app.use(cors());
+
+
+  app.options('*', cors());
+
+  app.use(express.json());
+
+  app.use("/api/tracker", trackerRoute);
+
+  const PORT = 3001;
+  app.listen(PORT, () => {
+    console.log("Sever running");
+  });
+};
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
