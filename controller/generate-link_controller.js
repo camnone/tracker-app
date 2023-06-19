ab17058068a9e5f37d28ae7babfa5b5b57d568ec
Artyom Tazhetdinov <artemtazhetdinov4@gmail.com>
2023-06-19 12:45:00 +0300

    update link

--- controller/generate-link_controller.js
+++ controller/generate-link_controller.js
@@ -38,7 +38,7 @@ export const generateLink = expressAsyncHandler(async (req, res) => {
 const redirect = async (res, geek_params) => {
   try {
     res.redirect(
-      `https://play.google.com/store/apps/details?id=${geek_params.g_app}?referrer=g_pid=${geek_params.g_pid}?g_c=${geek_params.g_c}?g_adset=${geek_params.g_adset}?userID=${geek_params.userID}`
+      `https://play.google.com/store/apps/details?id=${geek_params.g_app}&referrer=g_pid=${geek_params.g_pid}?g_c=${geek_params.g_c}?g_adset=${geek_params.g_adset}?userID=${geek_params.userID}`
     );
     res.status(200).send(geek_params);
   } catch (e) {
