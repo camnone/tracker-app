3a2eab9a1adf9518fe0b8c1b2acbeabdc022797f
Artyom Tazhetdinov <artemtazhetdinov4@gmail.com>
2023-06-19 17:15:02 +0300

    123

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const main = async () => {
 
   app.use("/api/tracker", trackerRoute);
 
-  const PORT = 80;
+  const PORT = 3000;
   app.listen(PORT, () => {
     console.log("Sever running");
   });
